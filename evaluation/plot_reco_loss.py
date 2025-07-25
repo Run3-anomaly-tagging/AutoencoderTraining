1a58d1f65c110ba13167205997e7a5cfc1d64ea3
Matej Roguljic <matej.roguljic@gmail.com>
2025-07-25 09:51:20 -0500

    add roc

--- evaluation/plot_reco_loss.py
+++ evaluation/plot_reco_loss.py
@@ -6,6 +6,7 @@ from AutoencoderTraining.paths import DEFAULT_MERGED_QCD_FILE, DEFAULT_SIGNAL_FI
 from AutoencoderTraining.paths import DENSE_AUTOENCODER_MODEL, IMAGE_AUTOENCODER_MODEL
 from AutoencoderTraining.utils.h5_helpers import extract_hidden_features, extract_jet_images
 import os
+from sklearn.metrics import roc_curve, auc
 
 N_PLOT=100000
 
@@ -51,3 +52,24 @@ for MODEL_PATH, extractor in zip([DENSE_AUTOENCODER_MODEL, IMAGE_AUTOENCODER_MOD
     out_path = f"evaluation/reco_loss_{model_name}.png"
     plt.savefig(out_path)
     print(f"Saved: {out_path}")
+
+
+    if model_name == "dense_autoencoder":#AUC needs to be implemented in image_ae first
+
+        y_true = np.concatenate([np.zeros_like(reco_bkg), np.ones_like(reco_sig)])
+        y_scores = np.concatenate([reco_bkg, reco_sig])
+
+        fpr, tpr, _ = roc_curve(y_true, y_scores)
+        auc_val = auc(fpr, tpr)
+
+        plt.figure(figsize=(6, 6))
+        plt.plot(fpr, tpr, label=f"AUC = {auc_val:.3f}")
+        plt.xlabel("Background mistag rate")
+        plt.ylabel("Signal efficiency")
+        plt.title("ROC Curve (Dense Autoencoder)")
+        plt.legend(loc="lower right")
+        plt.tight_layout()
+
+        out_path = "evaluation/roc_dense_autoencoder.png"
+        plt.savefig(out_path)
+        print(f"Saved: {out_path}")
